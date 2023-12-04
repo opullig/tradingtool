0e2eb07a188b370bcf3c6c6370021f3314eeefd0
Renan Pullig <contato@opullig.com.br>
2023-12-04 10:24:39 -0300

    Create Getters and access to class

--- client/app/models/trading/Trading.js
+++ client/app/models/trading/Trading.js
@@ -1,7 +1,25 @@
 class Trading{
-  constructor(){
-    this.data = new Date();
-    this.price = 0.0;
-    this.amount = 1;
+  constructor({date, price, quantity}){
+    this._date = new Date(date.getTime())
+    this._price = price
+    this._quantity = quantity
+
+    Object.freeze(this)
+  }
+
+  get amount(){
+    return this._price * this._quantity
+  }
+
+  get date(){
+    return new Date(  ) 
+  }
+
+  get quantity(){
+    return this._quantity
+  }
+
+  get price(){
+    return this._price
   }
 }
